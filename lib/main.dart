cb28b7f3f0aeed852eda490b005cf1d9523d0c7a
Shriganesh <shriganesh1987@gmail.com>
2021-02-05 19:38:59 +0530

    Completed Section 4, Chapther 87

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -13,29 +14,30 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
-              ),
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Column(
+        //mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('CHART!'),
+              elevation: 5,
             ),
-            Card(
-              color: Colors.red,
-              child: Text('LIST OF TX'),
-            ),
-          ],
-        ));
+          ),
+          UserTransactions(),
+        ],
+      ),
+    );
   }
 }
