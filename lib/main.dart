3e6dffd1e0591153a52c1e1fbe85784ba5229709
Shriganesh <shriganesh1987@gmail.com>
2021-02-14 00:20:54 +0530

    Completed: Section 5, Chapter 121

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:MyExpensePlanner/widgets/new_transaction.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './widgets/new_transaction.dart';
 import './models/transaction.dart';
 import './widgets/transaction_list.dart';
 import './widgets/chart.dart';
 
-void main() => runApp(MyApp());
+//void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
